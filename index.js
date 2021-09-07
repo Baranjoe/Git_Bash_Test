161c1a5c6732d39b8e0b79b4c7d6bf3f0cb25fe6
Barandun <j.barandun@lincolninternational.ch>
2021-09-07 16:12:53 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// COPY A FILE
+//
+// const fs = require('fs');
+//
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+var superheroes = require("superheroes");
+
+var mySuperHeroName = superheroes.random();
+
+console.log(mySuperHeroName);
+
+var supervillains = require("supervillains");
+
+var mySuperVillainName = supervillains.random();
+
+console.log(mySuperVillainName);
